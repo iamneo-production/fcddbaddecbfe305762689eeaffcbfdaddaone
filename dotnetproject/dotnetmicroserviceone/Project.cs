d44fa9b10931ac0180dfce2f5acff8e40705c822
Iamneo <techadmin@iamneo.ai>
2023-10-04 16:30:42 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Project.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetmicroserviceone
+{
+    public class Project
+    {
+        public int ProjectID{get;set;}
+        public string 
+    }
+}
---
+++ dotnetproject/dotnetmicroservicetwo/Freelancer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetmicroserviceone
+{
+    public class Freelancer
+    {
+        public int FreelancerID{get;set;}
+        public string FirtName{get;set;}
+        public string LastName{get;set;}
+        public string Email{get;set;}
+        public string Skills{get;set;}
+        public string Portfolio{get;set;}
+    }
+}
