bbeed2f19ada928e3be82d5f8b85be4ee6e78161
Iamneo <techadmin@iamneo.ai>
2023-10-04 17:55:56 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Migrations/20231004175336_initaldata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetmicroserviceone.Migrations
+{
+    /// <inheritdoc />
+    public partial class initaldata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Budget = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DeadLine = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.ProjectID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "projects");
+        }
+    }
+}
